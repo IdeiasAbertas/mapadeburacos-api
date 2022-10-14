309eea8da90f357b41b5585bcbbf4687d05ca644
kenneth luzolo <kennethluzolo@MacBook-Pro-de-Macbook-Pro.local>
2022-10-14 12:30:38 +0100

    Initial project setup

---
+++ src/main/java/ao/co/mapaDeBuraco/MapaDeBuracoApplication.java
@@ -0,0 +1,13 @@
+package ao.co.mapaDeBuraco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaDeBuracoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaDeBuracoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/co/mapaDeBuraco/MapaDeBuracoApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.co.mapaDeBuraco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaDeBuracoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
